a9134eeaf4a93613973575b36c77fb3a37189af1
Hà Vĩnh Phước <122790758+Kas2k3@users.noreply.github.com>
2023-10-16 21:58:24 +0700

    Them code ve goi trong java

---
+++ Bai47_Package/src/main/java/test/TestViDu.java
@@ -0,0 +1,17 @@
+package test;
+
+import code_a.ViDu;
+
+/**
+ *
+ * @author Phuoc
+ */
+public class TestViDu {
+    public static void main(String[] args) {
+        ViDu vd = new ViDu(5, 6);
+        System.out.println(vd.getA());
+        
+        code_b.ViDu vd2 = new code_b.ViDu(1, 2, 3);
+        System.out.println(vd2.getC());
+    }
+}
